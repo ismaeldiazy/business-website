c6083d7928064b772eac7a445b15dfeb523133ed
Ismael Díaz <ismael.diaz@airenetworks.es>
2020-05-26 20:32:23 +0200

    Give order number to the pages. Check if user is authenticated, if so, a edit button is displayed and the page can be edit from control panel

--- businessweb/pages/models.py
+++ businessweb/pages/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(verbose_name="Title", max_length=200)
     content = models.TextField(verbose_name="Content")
+    order = models.SmallIntegerField(verbose_name="Order", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
     updated = models.DateTimeField(auto_now=True, verbose_name="Updated date")
 
     class Meta:
         verbose_name = "page"
         verbose_name = "page"
-        ordering = ['title']
+        ordering = ['order','title']
     
     def __str__(self):
         return self.title
